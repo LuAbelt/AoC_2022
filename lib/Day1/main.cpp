76e3235391dab3cff4dc67a0c70998debd4b2a87
LuAbelt <l.abelt@luabelt.de>
2022-12-02 09:46:09 +0000

    * Finished Day 1 & 2

--- lib/Day1/main.cpp
+++ lib/Day1/main.cpp
@@ -1,9 +1,65 @@
+#include <algorithm>
 #include "lib.h"
 
+using namespace std;
 void part1(){
+    std::vector<std::vector<int64_t>> elfs;
+    std::vector<int64_t> sums;
+
+    size_t cur_idx = 0;
+    int64_t cur_sum = 0;
+    int64_t cur_max = 0;
+    elfs.push_back({});
+    sums.push_back(0);
+
+    for(string line;getline(cin,line);){
+        if(line.empty()){
+            cur_max = max(cur_max,cur_sum);
+            ++cur_idx;
+            cur_sum = 0;
+            elfs.push_back({});
+            sums.push_back(0);
+            continue;
+        }
+
+        int64_t val = atoi(line.c_str());
+        elfs[cur_idx].push_back(val);
+        sums[cur_idx] += val;
+        cur_sum += val;
+    }
+
+    cout << cur_max << endl;
 }
 
 void part2(){
+    std::vector<std::vector<int64_t>> elfs;
+    std::vector<int64_t> sums;
+
+    size_t cur_idx = 0;
+    int64_t cur_sum = 0;
+    int64_t cur_max = 0;
+    elfs.push_back({});
+    sums.push_back(0);
+
+    for(string line;getline(cin,line);){
+        if(line.empty()){
+            cur_max = max(cur_max,cur_sum);
+            ++cur_idx;
+            cur_sum = 0;
+            elfs.push_back({});
+            sums.push_back(0);
+            continue;
+        }
+
+        int64_t val = atoi(line.c_str());
+        elfs[cur_idx].push_back(val);
+        sums[cur_idx] += val;
+        cur_sum += val;
+    }
+
+    std::sort(sums.begin(), sums.end(), std::greater<>());
+
+    cout << std::accumulate(sums.begin(),std::next(sums.begin(),3),0,std::plus<>()) << endl;
 }
 
 int main(int argc, char* argv[]){
--- lib/Day2/main.cpp
+++ lib/Day2/main.cpp
@@ -1,9 +1,94 @@
+#include <map>
+#include <sstream>
 #include "lib.h"
+using namespace std;
+enum PlayChoice{
+    Rock,
+    Paper,
+    Scissors
+};
+
+map<PlayChoice,int> scores = {{PlayChoice::Rock,1},{PlayChoice::Paper,2},{PlayChoice::Scissors,3}};
+
+map<char,PlayChoice> choices = {{'A',PlayChoice::Rock},{'B',PlayChoice::Paper},{'C',PlayChoice::Scissors}
+,{'X',PlayChoice::Rock},{'Y',PlayChoice::Paper},{'Z',PlayChoice::Scissors}};
+
+int64_t get_outcome(PlayChoice opponent, PlayChoice me){
+    if (me==opponent){
+        return 3;
+    }
+
+    if(opponent==PlayChoice::Rock){
+        if(me==PlayChoice::Paper){
+            return 6;
+        }
+        return 0;
+    }
+
+    if(opponent==PlayChoice::Paper){
+        if(me==PlayChoice::Scissors){
+            return 6;
+        }
+        return 0;
+    }
+
+    if(opponent==PlayChoice::Scissors){
+        if(me==PlayChoice::Rock){
+            return 6;
+        }
+        return 0;
+    }
+}
 
 void part1(){
+    int64_t total_score = 0;
+    for(string line;getline(cin,line);){
+        stringstream ss(line);
+        char opponent, me;
+
+        ss >> opponent >> me;
+
+        total_score += scores[choices[me]];
+        total_score += get_outcome(choices[opponent],choices[me]);
+    }
+
+    cout << total_score << endl;
 }
 
 void part2(){
+    int64_t total_score = 0;
+
+    map<PlayChoice,PlayChoice> winsAgainst{{PlayChoice::Rock,PlayChoice::Paper}
+            ,{PlayChoice::Paper,PlayChoice::Scissors}
+            ,{PlayChoice::Scissors,PlayChoice::Rock}};
+
+    map<PlayChoice,PlayChoice> losesAgainst{{PlayChoice::Rock,PlayChoice::Scissors}
+            ,{PlayChoice::Paper,PlayChoice::Rock}
+            ,{PlayChoice::Scissors,PlayChoice::Paper}};
+
+    for(string line;getline(cin,line);){
+        stringstream ss(line);
+        char opponent, outcome;
+
+        ss >> opponent >> outcome;
+
+        switch (outcome) {
+            case 'X':
+                total_score += scores[losesAgainst[choices[opponent]]];
+                break;
+            case 'Y':
+                total_score += 3;
+                total_score += scores[choices[opponent]];
+                break;
+            case 'Z':
+                total_score += 6;
+                total_score += scores[winsAgainst[choices[opponent]]];
+                break;
+        }
+
+    }
+
+    cout << total_score << endl;
 }
 
 int main(int argc, char* argv[]){
