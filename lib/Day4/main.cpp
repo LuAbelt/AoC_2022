5c1942a885f35d0da143fec6894c9b643c3feddd
LuAbelt <l.abelt@luabelt.de>
2022-12-04 10:51:42 +0000

    * Finished Day4

--- lib/Day4/main.cpp
+++ lib/Day4/main.cpp
@@ -1,9 +1,41 @@
 #include "lib.h"
 
 void part1(){
+    i64 cnt = 0;
+    for(string line; getline(cin,line);){
+        string sub1 = line.substr(0,line.find(","));
+        string sub2 = line.substr(line.find(",")+1);
+
+        auto begin1 = atoi(sub1.substr(0,sub1.find("-")).c_str());
+        auto end1 = atoi(sub1.substr(sub1.find("-")+1).c_str());
+
+        auto begin2 = atoi(sub2.substr(0,sub2.find("-")).c_str());
+        auto end2 = atoi(sub2.substr(sub2.find("-")+1).c_str());
+
+        if((begin1<=begin2 && end1>=end2)||(begin2<=begin1 && end2>=end1)){
+            ++cnt;
+        }
+    }
+    cout << cnt << endl;
 }
 
 void part2(){
+    i64 cnt = 0;
+    for(string line; getline(cin,line);){
+        string sub1 = line.substr(0,line.find(","));
+        string sub2 = line.substr(line.find(",")+1);
+
+        auto begin1 = atoi(sub1.substr(0,sub1.find("-")).c_str());
+        auto end1 = atoi(sub1.substr(sub1.find("-")+1).c_str());
+
+        auto begin2 = atoi(sub2.substr(0,sub2.find("-")).c_str());
+        auto end2 = atoi(sub2.substr(sub2.find("-")+1).c_str());
+
+        if((begin1<=begin2 && end1>=begin2)||(begin2<=begin1 && end2>=begin1)){
+            ++cnt;
+        }
+    }
+    cout << cnt << endl;
 }
 
 int main(int argc, char* argv[]){
